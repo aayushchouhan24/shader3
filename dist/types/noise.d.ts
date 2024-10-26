e1cda952143666db38f7679349cf83936c0c25f0
Aayush Chouhan <aayushchouhan24@gmail.com>
2024-10-26 23:09:40 +0530

    added new voronoi

--- dist/types/noise.d.ts
+++ dist/types/noise.d.ts
@@ -2,4 +2,5 @@ import truchetPattern from './shaders/includes/truchetPattern.glsl';
 import perlin from './shaders/includes/perlin.glsl';
 import fbm from './shaders/includes/fbm.glsl';
 import simplex from './shaders/includes/simplex.glsl';
-export { truchetPattern, perlin, fbm, simplex };
+import voronoi from './shaders/includes/voronoi.glsl';
+export { truchetPattern, perlin, fbm, simplex, voronoi };
